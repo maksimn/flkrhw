a37bb8da526307606777044e09570b9e35a7e84a
Maksim Ivanov <maksim1989sovunion@yandex.ru>
2019-04-19 13:47:48 +0300

    init

---
+++ flkrhw/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  flkrhw
+//
+//  Created by Maksim Ivanov on 19/04/2019.
+//  Copyright © 2019 Maksim Ivanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
