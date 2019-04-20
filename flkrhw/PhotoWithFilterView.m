d89a69a29a423413380d5d54cde4c5a6a3db5f6f
Maksim Ivanov <maksim1989sovunion@yandex.ru>
2019-04-20 11:52:20 +0300

    PhotoWithFilterView back button

--- flkrhw/PhotoWithFilterView.m
+++ flkrhw/PhotoWithFilterView.m
@@ -8,20 +8,43 @@
 
 #import "PhotoWithFilterView.h"
 
+
+@interface PhotoWithFilterView ()
+
+@property (nonatomic, strong) UIButton *backButton;
+
+@end
+
+
 @implementation PhotoWithFilterView
 
 - (instancetype)initWithImage:(UIImage *) image
 {
-    CGRect frame = CGRectMake(0, 0, UIScreen.mainScreen.bounds.size.width, UIScreen.mainScreen.bounds.size.height);
+    CGFloat screenWidth = UIScreen.mainScreen.bounds.size.width;
+    CGFloat screenHeight = UIScreen.mainScreen.bounds.size.height;
+    CGRect frame = CGRectMake(0, 0, screenWidth, screenHeight);
+    
     if (self = [super initWithFrame:frame])
     {
         UIImageView *selectedImageView = [[UIImageView alloc] initWithFrame:frame];
         
         [selectedImageView setImage:image];
         [self addSubview:selectedImageView];
+        
+        self.backButton = [UIButton buttonWithType:UIButtonTypeSystem];
+        self.backButton.frame = CGRectMake(20.0, screenHeight - 40.0, 80.0, 30.0);
+        self.backButton.backgroundColor = [UIColor cyanColor];
+        [self.backButton setTitle:@"Назад" forState:UIControlStateNormal];
+        [self.backButton addTarget:self action:@selector(hideThisView) forControlEvents:UIControlEventTouchUpInside];
+        [self addSubview: self.backButton];
     }
     
     return self;
+}
+
+- (void)hideThisView
+{
+    [self removeFromSuperview];
 }
 
 @end
