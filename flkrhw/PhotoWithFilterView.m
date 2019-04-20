835198110434a3fe47f05322e709d866f76edb97
Maksim Ivanov <maksim1989sovunion@yandex.ru>
2019-04-20 15:27:37 +0300

    Invert filter

--- flkrhw/PhotoWithFilterView.m
+++ flkrhw/PhotoWithFilterView.m
@@ -14,7 +14,7 @@
 @property (nonatomic, strong) UIImageView *imageView;
 @property (nonatomic, strong) UIButton *backButton;
 @property (nonatomic, strong) UIButton *filterButton;
-@property (nonatomic, strong) UIImageView *resultImageView;
+@property (nonatomic, strong) UIButton *invertFilterButton;
 
 @end
 
@@ -53,8 +53,15 @@
     self.filterButton.frame = CGRectMake(110.0, UIScreen.mainScreen.bounds.size.height - 40.0, 80.0, 30.0);
     self.filterButton.backgroundColor = [UIColor cyanColor];
     [self.filterButton setTitle:@"Сепия" forState:UIControlStateNormal];
-    [self.filterButton addTarget:self action:@selector(doFilter) forControlEvents:UIControlEventTouchUpInside];
+    [self.filterButton addTarget:self action:@selector(doSepiaFilter) forControlEvents:UIControlEventTouchUpInside];
     [self addSubview: self.filterButton];
+    
+    self.invertFilterButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    self.invertFilterButton.frame = CGRectMake(200.0, UIScreen.mainScreen.bounds.size.height - 40.0, 80.0, 30.0);
+    self.invertFilterButton.backgroundColor = [UIColor cyanColor];
+    [self.invertFilterButton setTitle:@"Инверсия" forState:UIControlStateNormal];
+    [self.invertFilterButton addTarget:self action:@selector(doInvertFilter) forControlEvents:UIControlEventTouchUpInside];
+    [self addSubview: self.invertFilterButton];
 }
 
 - (void)hideThisView
@@ -62,25 +69,32 @@
     [self removeFromSuperview];
 }
 
-- (void)doFilter
+- (void)doSepiaFilter
 {
     UIImage *image = [self.imageView image];
     CIImage *ciImage = [CIImage imageWithCGImage:image.CGImage];
-    
-    CIContext *context = [CIContext contextWithOptions:nil];
-    
     CIFilter *filter = [CIFilter filterWithName:@"CISepiaTone" keysAndValues: kCIInputImageKey, ciImage, @"inputIntensity", @0.8, nil];
+    [self doFilter:filter];
+}
+
+- (void)doInvertFilter
+{
+    UIImage *image = [self.imageView image];
+    CIImage *ciImage = [CIImage imageWithCGImage:image.CGImage];
+    CIFilter *filter = [CIFilter filterWithName:@"CIColorInvert" keysAndValues: kCIInputImageKey, ciImage, nil];
+    [self doFilter:filter];
+}
+
+- (void)doFilter:(CIFilter *)filter
+{
+    CIContext *context = [CIContext contextWithOptions:nil];
     CIImage *outputImage = [filter outputImage];
-    
     // Создаем Bitmap
     CGImageRef cgimg = [context createCGImage:outputImage fromRect:[outputImage extent]];
-    
     // Создаем изображение
     UIImage *newImage = [UIImage imageWithCGImage:cgimg];
-    
     // Релизим ImageRef, потому как там старое C API, нужно ручками
     CGImageRelease(cgimg);
-    
     [self.imageView removeFromSuperview];
     [self.backButton removeFromSuperview];
     [self.filterButton removeFromSuperview];
