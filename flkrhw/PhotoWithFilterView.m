15be2cc2140a6b09dc540e4d75c668ca0da54ddf
Maksim Ivanov <maksim1989sovunion@yandex.ru>
2019-04-20 14:23:19 +0300

    Сепия

--- flkrhw/PhotoWithFilterView.m
+++ flkrhw/PhotoWithFilterView.m
@@ -14,6 +14,7 @@
 @property (nonatomic, strong) UIImageView *imageView;
 @property (nonatomic, strong) UIButton *backButton;
 @property (nonatomic, strong) UIButton *filterButton;
+@property (nonatomic, strong) UIImageView *resultImageView;
 
 @end
 
@@ -28,27 +29,32 @@
     
     if (self = [super initWithFrame:frame])
     {
-        self.imageView = [[UIImageView alloc] initWithFrame:frame];
-        
-        [self.imageView setImage:image];
-        [self addSubview:self.imageView];
-        
-        self.backButton = [UIButton buttonWithType:UIButtonTypeSystem];
-        self.backButton.frame = CGRectMake(20.0, screenHeight - 40.0, 80.0, 30.0);
-        self.backButton.backgroundColor = [UIColor cyanColor];
-        [self.backButton setTitle:@"Назад" forState:UIControlStateNormal];
-        [self.backButton addTarget:self action:@selector(hideThisView) forControlEvents:UIControlEventTouchUpInside];
-        [self addSubview: self.backButton];
-        
-        self.filterButton = [UIButton buttonWithType:UIButtonTypeSystem];
-        self.filterButton.frame = CGRectMake(110.0, screenHeight - 40.0, 80.0, 30.0);
-        self.filterButton.backgroundColor = [UIColor cyanColor];
-        [self.filterButton setTitle:@"Фильтр" forState:UIControlStateNormal];
-        [self.filterButton addTarget:self action:@selector(doFilter) forControlEvents:UIControlEventTouchUpInside];
-        [self addSubview: self.filterButton];
+        [self prepareUI:image frame:frame];
     }
     
     return self;
+}
+
+- (void)prepareUI:(UIImage *)img frame:(CGRect) frame
+{
+    self.imageView = [[UIImageView alloc] initWithFrame:frame];
+    
+    [self.imageView setImage:img];
+    [self addSubview:self.imageView];
+    
+    self.backButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    self.backButton.frame = CGRectMake(20.0, UIScreen.mainScreen.bounds.size.height - 40.0, 80.0, 30.0);
+    self.backButton.backgroundColor = [UIColor cyanColor];
+    [self.backButton setTitle:@"Назад" forState:UIControlStateNormal];
+    [self.backButton addTarget:self action:@selector(hideThisView) forControlEvents:UIControlEventTouchUpInside];
+    [self addSubview: self.backButton];
+    
+    self.filterButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    self.filterButton.frame = CGRectMake(110.0, UIScreen.mainScreen.bounds.size.height - 40.0, 80.0, 30.0);
+    self.filterButton.backgroundColor = [UIColor cyanColor];
+    [self.filterButton setTitle:@"Сепия" forState:UIControlStateNormal];
+    [self.filterButton addTarget:self action:@selector(doFilter) forControlEvents:UIControlEventTouchUpInside];
+    [self addSubview: self.filterButton];
 }
 
 - (void)hideThisView
@@ -58,7 +64,27 @@
 
 - (void)doFilter
 {
-    NSLog(@"do filter");
+    UIImage *image = [self.imageView image];
+    CIImage *ciImage = [CIImage imageWithCGImage:image.CGImage];
+    
+    CIContext *context = [CIContext contextWithOptions:nil];
+    
+    CIFilter *filter = [CIFilter filterWithName:@"CISepiaTone" keysAndValues: kCIInputImageKey, ciImage, @"inputIntensity", @0.8, nil];
+    CIImage *outputImage = [filter outputImage];
+    
+    // Создаем Bitmap
+    CGImageRef cgimg = [context createCGImage:outputImage fromRect:[outputImage extent]];
+    
+    // Создаем изображение
+    UIImage *newImage = [UIImage imageWithCGImage:cgimg];
+    
+    // Релизим ImageRef, потому как там старое C API, нужно ручками
+    CGImageRelease(cgimg);
+    
+    [self.imageView removeFromSuperview];
+    [self.backButton removeFromSuperview];
+    [self.filterButton removeFromSuperview];
+    [self prepareUI:newImage frame:CGRectMake(0, 0, UIScreen.mainScreen.bounds.size.width, UIScreen.mainScreen.bounds.size.height)];
 }
 
 @end
